691e32afb686be6e74fdba9473863147a0e73879
alissonph <37026901+alissonph@users.noreply.github.com>
2021-01-30 15:03:50 -0300

    Desenvolvimento tela principal

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Container, Section, SectionHeader, SectionContent, Avatar, SectionName, LastName, Name, Function } from './styles';
-import perfil from '../../assets/images/perfil.jpg'
+import { Container, Section, SectionHeader, SectionContent, Avatar, SectionName, SectionLink, LinkButton, SectionInfo, InfoItem} from './styles';
+import perfil from '../../assets/images/perfil.jpg';
+import linkedin from '../../assets/icons/linkedin.svg';
+import github from '../../assets/icons/github.svg';
 
 export default function Page() {
   return (
@@ -11,14 +13,40 @@ export default function Page() {
         </SectionHeader>
         <SectionContent>
           <SectionName>
-            <LastName>HAUSMANN</LastName>
-            <Name>ALISSON</Name>
-            <Function>Desenvolvedor de Software Pleno</Function>
+            <p className="lastName">HAUSMANN</p>
+            <p className="name">ALISSON</p>
+            <p className="function">Desenvolvedor de Software Pleno</p>
           </SectionName>
+          <SectionLink>
+            <LinkButton href="https://www.linkedin.com/in/alissonph" target="_blank">
+              <img src={linkedin} alt="Linkedin"/>
+            </LinkButton>
+            <LinkButton href="https://github.com/alissonph" target="_blank"n>
+              <img src={github} alt="Github"/>
+            </LinkButton>
+          </SectionLink>
+          <SectionInfo>
+            <InfoItem>
+              <p className="title">Localização</p>
+              <p className="description">Artur Nogueira - SP</p>
+            </InfoItem>
+            <InfoItem>
+              <p className="title">Telefone</p>
+              <p className="description">(19) 99117-0250</p>
+            </InfoItem>
+            <InfoItem>
+              <p className="title">E-mail</p>
+              <p className="description">alissonphausmann@hotmail.com</p>
+            </InfoItem>
+            <InfoItem>
+              <p className="title">Linkedin</p>
+              <p className="description">www.linkedin.com/in/alissonph</p>
+            </InfoItem>
+          </SectionInfo>
         </SectionContent>
       </Section>
       <Section>
-        Teste Alisson
+        TESTE
       </Section>
     </Container>
   )
--- src/components/Page/styles.js
+++ src/components/Page/styles.js
@@ -1,37 +1,35 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+
 `;
 
 export const Section = styled.div`
   display: flex;
   flex-direction: row;
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   background-color: green;
-  align-self: center;
+
+  
 `;
 
 export const SectionHeader = styled.div`
   display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  align-items: center;
-  width: 50%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+  width: 40%;
   background-color: #2b2c2c;
   padding-right: 50px;
 `;
 
 export const SectionContent = styled.div`
   display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-content: center;
-  align-items: center;
-  width: 50%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  width: 60%;
   background-color: #343636;
   padding-left: 50px;
 `;
@@ -46,19 +44,73 @@ export const Avatar = styled.img`
 export const SectionName = styled.div`
   display: flex;
   flex-direction: column;
+  
+  .lastName{
+    font-size: 30px;
+  }
+  .name{
+    font-size: 80px;
+    font-weight: bold;
+    color: #e0a80d;
+  }
+  .function{
+    padding-top: 10px;
+    font-size: 20px;
+  }
 `;
 
-export const LastName = styled.p`
-  font-size: 30px;
+export const SectionLink = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top:10px;
 `;
 
-export const Name = styled.p`
-  font-size: 80px;
-  font-weight: bold;
-  color: #e0a80d;
+export const LinkButton = styled.a`
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 3px;
+  width: 35px;
+  height: 35px;
+  border-radius: 6px;
+  cursor: pointer;
+  background-color: #2b2c2c;
+
+  img{
+    width: 25px;
+    height: 25px;
+    filter: invert(73%) sepia(4%) saturate(85%) hue-rotate(169deg) brightness(92%) contrast(84%);
+  }
+
+  :hover{
+    img {
+      width: 27px;
+      height: 27px;
+      filter: invert(83%) sepia(47%) saturate(4126%) hue-rotate(1deg) brightness(94%) contrast(90%);
+    }
+  }
 `;
 
-export const Function = styled.p`
-  padding-top: 10px;
-  font-size: 20px;
+export const SectionInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  bottom: 30px;
+  height: 100px;
+  border-top: 1px solid #CCC;
+`;
+
+export const InfoItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+
+  .title{
+    font-size: 16px;
+    font-weight: bold;
+  }
+  .description{
+    margin-top: 7px;
+    font-size: 12px;
+  }
 `;
